9ac5a38b13cb97165b3d27d8d25437bd9f8b62a9
Glenn Ivander Jano <ivander792@gmail.com>
2025-11-25 00:14:18 +0800

    update task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'due_date',
+    ];
 }
